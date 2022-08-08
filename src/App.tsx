9e8babf06ab16d05f0a288b1b79e7ebcc7f38de7
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-08-07 21:49:50 -0700

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-import * as solanaWeb3 from '@solana/web3.js';
+import * as Web3 from '@solana/web3.js';
 
 function App(props: any) {
   const [count, setCount] = useState(0)
 
   const handleCheckBalance = async (address: string): Promise<number> => {
-    const key: solanaWeb3.PublicKey = new solanaWeb3.PublicKey(address);
-    const connection = new solanaWeb3.Connection(solanaWeb3.clusterApiUrl('devnet'));
+    const key: Web3.PublicKey = new Web3.PublicKey(address);
+    const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
     return connection.getBalance(key).then(balance => {
-      console.log('Balance: ', (balance / solanaWeb3.LAMPORTS_PER_SOL))
+      console.log('Balance: ', (balance / Web3.LAMPORTS_PER_SOL))
       return balance;
     });
   }
